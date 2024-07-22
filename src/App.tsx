3706ebc7951a88050271fbdddab57fa0926c71d0
Nikolay <coyg1414@gmail.com>
2024-07-22 06:42:07 +0300

    add chadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@/components/ui/button"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Button>Click me</Button>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </>
   )
 }
