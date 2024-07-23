f23e98d9b92758d5d52138d8469c54e46d26c1f2
Nikolay <coyg1414@gmail.com>
2024-07-23 06:01:26 +0300

    add supbase ro project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/ui/button"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 import './App.css'
 
@@ -7,11 +5,7 @@ function App() {
 
   return (
     <>
-      <Button>Click me</Button>
-      <Avatar>
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+
     </>
   )
 }
