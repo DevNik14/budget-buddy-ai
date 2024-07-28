828efbd65c652e14115ab57afa6298b5a2c14e5b
Nikolay <coyg1414@gmail.com>
2024-07-28 18:27:41 +0300

    add basic layout for registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { app, auth, createUserWithEmailAndPassword } from "./config/firebase";
+import RegisterFormPage from "./pages/Login";
 
 function App() {
-  console.log(createUserWithEmailAndPassword);
-
-  return <></>;
+  return (
+    <>
+      <RegisterFormPage />
+      <footer className="absolute bottom-0 text-center w-full">
+        Built with &hearts; by{" "}
+        <a href="https://github.com/DevNik14" target="_blank">
+          Nikolay Kyurchiyski
+        </a>
+      </footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Login/LoginForm.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { FaGoogle } from "react-icons/fa6";
+
+import styles from "./LoginForm.module.css";
+
+export default function RegisterForm(): React.JSX.Element {
+  const [message, setMessage] = useState("");
+
+  const googleLoginClickHandler = () => {
+    setMessage("Still in development");
+
+    setTimeout(() => {
+      setMessage("");
+    }, 3000);
+  };
+
+  return (
+    <>
+      <main>
+        <form>
+          <Input type="email" placeholder="name@example.com" />
+          <Input type="password" placeholder="Password" />
+          <Button>Sign in with email</Button>
+        </form>
+        <span>Or continue with</span>
+        {message !== "" && <p>{message}</p>}
+        <Button onClick={googleLoginClickHandler}>
+          <FaGoogle />
+          Google
+        </Button>
+      </main>
+    </>
+  );
+}
