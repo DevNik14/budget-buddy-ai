39e8b1025db689decf36185770c11a11ba444959
Nikolay Kyurchiyski <nykyurchiyski@gmail.com>
2024-08-18 18:33:18 +0300

    4 set up basic authentication UI components (#9)
    feat: add labels and sign up link

--- src/pages/Login/LoginForm.tsx
+++ src/pages/Login/LoginForm.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { GoolgeSvg } from "@/assets/google";
+import { Link } from "react-router-dom";
 
 export default function LoginForm(): React.JSX.Element {
   const [message, setMessage] = useState("");
@@ -21,12 +22,20 @@ export default function LoginForm(): React.JSX.Element {
           <div className="bg-[url('@/assets/auth-bg.jpg')] bg-no-repeat bg-cover bg-center h-full w-full"></div>
           <div className="flex flex-col justify-center items-center h-screen w-full">
             <div className="flex flex-col w-4/6 text-center gap-y-3">
+              <label htmlFor="email" className="text-left">
+                Email
+              </label>
               <Input
+                id="email"
                 type="email"
                 placeholder="name@example.com"
                 className="rounded"
               />
+              <label htmlFor="password" className="text-left">
+                Password
+              </label>
               <Input
+                id="password"
                 type="password"
                 placeholder="Password"
                 className="rounded"
@@ -47,6 +56,12 @@ export default function LoginForm(): React.JSX.Element {
                 <GoolgeSvg />
                 <span className="ml-2">Google</span>
               </Button>
+              <p>
+                Don't have an account?{" "}
+                <strong>
+                  <Link to="/register">Sign up here</Link>
+                </strong>
+              </p>
             </div>
           </div>
         </div>
