5e6b7391a39ee0d1dfe0795e4390e4911dc1fcab
DevNik14 <nykyurchiyski@gmail.com>
2024-08-19 07:56:43 +0300

    style: add bsic styling to the register page

--- src/pages/Register/RegisterForm.tsx
+++ src/pages/Register/RegisterForm.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { FaGoogle } from "react-icons/fa6";
+import { useState } from "react";
+import { GoolgeSvg } from "@/assets/google";
+import { Link } from "react-router-dom";
 
 export default function RegisterForm(): React.JSX.Element {
   const [message, setMessage] = useState("");
@@ -18,17 +18,70 @@ export default function RegisterForm(): React.JSX.Element {
   return (
     <>
       <main>
-        <form>
-          <Input type="email" placeholder="name@example.com" />
-          <Input type="password" placeholder="Password" />
-          <Button>Sign in with email</Button>
-        </form>
-        <span>Or continue with</span>
-        {message !== "" && <p>{message}</p>}
-        <Button onClick={googleLoginClickHandler}>
-          <FaGoogle />
-          Google
-        </Button>
+        <div className="bg-[#F5F1EA]">
+          <div className="flex flex-col justify-center items-center h-screen w-full">
+            <div className="flex flex-col w-[540px]  text-center gap-y-2 bg-[#fff] rounded-[10px] px-10 py-10">
+              <label htmlFor="email" className="text-left">
+                Email
+              </label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="name@example.com"
+                className="rounded"
+              />
+              <label htmlFor="password" className="text-left">
+                Password
+              </label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="Password"
+                className="rounded"
+              />
+              <label htmlFor="firstName" className="text-left">
+                First Name
+              </label>
+              <Input
+                id="firstName"
+                type="text"
+                placeholder="First Name"
+                className="rounded"
+              />
+              <label htmlFor="lastName" className="text-left">
+                last Name
+              </label>
+              <Input
+                id="lastName"
+                type="text"
+                placeholder="Last Name"
+                className="rounded"
+              />
+              <Button
+                className="rounded border-solid border-black"
+                variant="outline"
+              >
+                Sign up
+              </Button>
+              <span>Or continue with</span>
+              {message !== "" && <p>{message}</p>}
+              <Button
+                onClick={googleLoginClickHandler}
+                className="w-full rounded flex justify-items-center"
+                variant="outline"
+              >
+                <GoolgeSvg />
+                <span className="ml-2">Google</span>
+              </Button>
+              <p>
+                Already a user?{" "}
+                <strong>
+                  <Link to="/login">Sign in</Link>
+                </strong>
+              </p>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
