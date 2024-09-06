7ae00f21e27029373e400bfe68a6f3b49781ec6e
Nikolay Kyurchiyski <nykyurchiyski@gmail.com>
2024-09-06 21:04:00 +0300

    23 create an api service function to send registration data to firebase (#29)
    test: add lazy initialization and useAuth hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import LoginForm from "./pages/Login/LoginForm";
 import RegisterForm from "./pages/Register/RegisterForm";
 import { useContext } from "react";
-import { AuthContext } from "./contexts/authContext";
+import { AuthContext, useAuth } from "./contexts/authContext";
 
 function App() {
-  const authContext = useContext(AuthContext);
+  const auth = useAuth();
   return (
     <>
       <Routes>
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
-        <Route path="/" element={<h1>Hello, {authContext?.user?.email}</h1>} />
+        <Route path="/" element={<h1>Hello, {auth.email}</h1>} />
       </Routes>
     </>
   );
