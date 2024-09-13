52180868d4f286d1bc4c950a71a72ca4c9d63322
Nikolay Kyurchiyski <nykyurchiyski@gmail.com>
2024-09-13 10:03:15 +0300

    36 implement side navigation (#38)
    fix: fix the flickering when there's no user in state by adding authenticated state

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import { signOut } from "firebase/auth";
-import { FirebaseError } from "firebase/app";
 
 import LoginForm from "./pages/Login/LoginForm";
 import RegisterForm from "./pages/Register/RegisterForm";
 import Layout from "./pages/Layout/Layout";
 
-import { auth } from "./config/firebase";
 import { useAuth } from "./contexts/authContext";
 
 function App() {
   const user = useAuth();
-  const hello = user ? <h1>hello, {user.email}</h1> : <h1>hello, stranger</h1>;
+  const hello = user ? (
+    <h1>hello, {user.user?.email}</h1>
+  ) : (
+    <h1>hello, stranger</h1>
+  );
   return (
     <>
       <Routes>
@@ -21,21 +22,6 @@ function App() {
           <Route path="/" element={hello} />
         </Route>
       </Routes>
-      <button
-        onClick={() => {
-          signOut(auth)
-            .then(() => {
-              console.log("Signed out");
-            })
-            .catch((error) => {
-              if (error instanceof FirebaseError) {
-                console.log(error);
-              }
-            });
-        }}
-      >
-        Sign out
-      </button>
     </>
   );
 }
