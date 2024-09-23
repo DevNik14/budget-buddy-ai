76d1bf8834169f05cc20d9da8bb9af30679b6c75
DevNik14 <nykyurchiyski@gmail.com>
2024-09-23 18:49:12 +0300

    feat: add user initials, user email and user name in the user panel

--- src/components/ui/NavAvatar.tsx
+++ src/components/ui/NavAvatar.tsx
@@ -1,16 +1,41 @@
+import { useAuth } from "@/contexts/authContext";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useEffect, useState } from "react";
 
 export default function NavAvatar() {
+  const { user } = useAuth();
+  const [userInfo, setUserInfo] = useState({
+    photoURL: "",
+    displayName: "",
+    email: "",
+    userInitials: "",
+  });
+
+  useEffect(() => {
+    if (user?.email) {
+      const { displayName, email, photoURL } = user;
+      let userName = displayName ? displayName : user.email.split("@")[0];
+      const userInitials = `${userName[0]}${userName[userName.length - 1]}`;
+
+      setUserInfo({
+        photoURL,
+        email,
+        displayName: userName,
+        userInitials,
+      });
+    }
+  }, [user]);
+
   return (
     <div className="p-5">
       <div className="flex items-center space-x-3">
         <Avatar>
-          <AvatarImage src="/placeholder.svg?height=40&width=40" alt="User" />
-          <AvatarFallback>JD</AvatarFallback>
+          <AvatarImage src={userInfo.photoURL} alt="User" />
+          <AvatarFallback>{userInfo.userInitials}</AvatarFallback>
         </Avatar>
         <div>
-          <h2 className="font-semibold">John Doe</h2>
-          <p className="text-sm text-gray-500">john@example.com</p>
+          <h2 className="font-semibold">{userInfo.displayName}</h2>
+          <p className="text-sm text-gray-500">{userInfo.email}</p>
         </div>
       </div>
     </div>
