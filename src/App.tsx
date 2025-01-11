8c1d165b26169dbfff69f11a08217194f60ca545
DevNik14 <nykyurchiyski@gmail.com>
2025-01-11 10:49:41 +0200

    add RHF to project and fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import { Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout/Layout";
 import DashboardPage from "./pages/Dashboard";
-import LoginForm from "./pages/Login/LoginForm";
-import RegisterForm from "./pages/Register/RegisterForm";
 import TransactionsPage from "./pages/Transactions";
 import BudgetPage from "./pages/Budget";
 import AdvisorPage from "./pages/Advisor";
 import NotFoundPage from "./pages/NotFound";
 import UserSettingsPage from "./pages/UserSettings";
+import LoginFormPage from "./pages/Login";
+import RegisterFormPage from "./pages/Register";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginFormPage />} />
+        <Route path="/register" element={<RegisterFormPage />} />
         <Route element={<Layout />}>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/transactions" element={<TransactionsPage />} />
