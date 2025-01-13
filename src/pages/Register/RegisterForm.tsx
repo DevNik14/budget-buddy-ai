39ecf5dd50393df6edf4b7c17c1dcb41f9bfb8af
Nikolay Kyurchiyski <nykyurchiyski@gmail.com>
2025-01-13 16:11:44 +0200

    style(register): align styling with login page for consistency (#67)

--- src/pages/Register/RegisterForm.tsx
+++ src/pages/Register/RegisterForm.tsx
@@ -7,6 +7,8 @@ import { FirebaseError } from "firebase/app";
 
 import { useAuth } from "@/contexts/authContext";
 
+import { cn } from "@/lib/utils";
+
 import ErrorAuthMessage from "@/components/ui/ErrorAuthMessage";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -19,7 +21,10 @@ type Inputs = {
   password: string;
 };
 
-export default function RegisterForm(): React.JSX.Element {
+export default function RegisterForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"form">): React.JSX.Element {
   const {
     register,
     handleSubmit,
@@ -62,14 +67,20 @@ export default function RegisterForm(): React.JSX.Element {
     createUserHandler(data);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
-      <div className="bg-[#0047AB]"></div>
-      <div className="flex flex-col justify-center items-center h-screen w-full ">
-        <div className="h-12 mb-4">
-          {error !== "" && <ErrorAuthMessage message={error} />}
+    <>
+      <div className="h-12 mb-4 errorMessage">
+        {error !== "" && <ErrorAuthMessage message={error} />}
+      </div>
+      <form
+        onSubmit={handleSubmit(submitHandler)}
+        className={cn("flex flex-col gap-6", className)}
+        {...props}
+      >
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Sign up</h1>
         </div>
-        <div className="flex flex-col w-4/6 gap-y-3">
-          <form onSubmit={handleSubmit(submitHandler)}>
+        <div className="grid gap-6">
+          <div className="grid gap-2 relative ">
             <label htmlFor="email" className="text-left">
               Email
             </label>
@@ -86,15 +97,25 @@ export default function RegisterForm(): React.JSX.Element {
               })}
             />
             {errors.email && (
-              <div className="text-white text-xs flex flex-start mt-[0.25rem]">
+              <div className="text-white text-xs flex flex-start mt-[0.25rem] absolute bottom-[-21px]">
                 <div className="py-[0.125rem] px-[0.25rem] bg-red-600 rounded">
                   {errors.email.message}
                 </div>
               </div>
             )}
-            <label htmlFor="password" className="text-left">
-              Password
-            </label>
+          </div>
+          <div className="grid gap-2 relative">
+            <div className="flex items-center">
+              <label htmlFor="password" className="text-left">
+                Password
+              </label>
+              <Link
+                to="/register"
+                className="ml-auto text-sm underline-offset-4 hover:underline"
+              >
+                Forgot your password?
+              </Link>
+            </div>
             <Input
               className="rounded"
               type="password"
@@ -108,42 +129,44 @@ export default function RegisterForm(): React.JSX.Element {
               })}
             />
             {errors.password && (
-              <div className="text-white text-xs flex flex-start my-[0.25rem]">
+              <div className="text-white text-xs flex flex-start my-[0.25rem] absolute bottom-[-25px]">
                 <div className="py-[0.125rem] px-[0.25rem] bg-red-600 rounded">
                   {errors.password.message}
                 </div>
               </div>
             )}
-            <div className="text-center">
-              <Button
-                className="rounded border-solid border-black  w-full"
-                variant="outline"
-                type="submit"
-              >
-                Create account
-              </Button>
-            </div>
-          </form>
-          <div className="text-center">
-            <span>Or continue with</span>
           </div>
-          {message !== "" && <p>{message}</p>}
-          <Button
-            onClick={googleLoginClickHandler}
-            className="w-full rounded flex justify-items-center"
-            variant="outline"
-          >
-            <GoolgeSvg />
-            <span className="ml-2">Google</span>
-          </Button>
           <div className="text-center">
-            Already a user?{" "}
-            <strong>
-              <Link to="/login">Sign in</Link>
-            </strong>
+            <Button
+              className="rounded border-solid border-black  w-full"
+              variant="outline"
+              type="submit"
+            >
+              Create account
+            </Button>
           </div>
         </div>
+        <div className="relative text-center text-sm">
+          <span className="relative z-10 bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </form>
+      {message !== "" && <p>{message}</p>}
+      <Button
+        onClick={googleLoginClickHandler}
+        className="w-full rounded flex justify-items-center mb-[1.5rem] mt-[1.5rem]"
+        variant="outline"
+      >
+        <GoolgeSvg />
+        <span className="ml-2">Google</span>
+      </Button>
+      <div className="text-center text-sm">
+        Already a user?{" "}
+        <strong>
+          <Link to="/login">Sign in</Link>
+        </strong>
       </div>
-    </div>
+    </>
   );
 }
