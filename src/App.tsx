fa07c409665bebcd75b7ee0e663222a6dfb91642
DevNik14 <nykyurchiyski@gmail.com>
2025-01-16 13:44:58 +0200

    chore: rename "Transactions" to "Expenses" in title and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Layout from "./pages/Layout/Layout";
 import DashboardPage from "./pages/Dashboard";
-import TransactionsPage from "./pages/Transactions";
+import ExpensesPage from "./pages/Expenses";
 import BudgetPage from "./pages/Budget";
 import AdvisorPage from "./pages/Advisor";
 import NotFoundPage from "./pages/NotFound";
@@ -18,7 +18,7 @@ function App() {
         <Route path="/register" element={<RegisterFormPage />} />
         <Route element={<Layout />}>
           <Route path="/" element={<DashboardPage />} />
-          <Route path="/transactions" element={<TransactionsPage />} />
+          <Route path="/expenses" element={<ExpensesPage />} />
           <Route path="/budget" element={<BudgetPage />} />
           <Route path="/advisor" element={<AdvisorPage />} />
           <Route path="/settings" element={<UserSettingsPage />} />
--- src/pages/Transactions/index.tsx
+++
@@ -1,5 +0,0 @@
-import Transactions from "./Transactions";
-
-export default function TransactionsPage() {
-  return <Transactions />;
-}
