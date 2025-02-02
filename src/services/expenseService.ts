2c4850d05f206492b65c4798edb7f4929e67e706
DevNik14 <nykyurchiyski@gmail.com>
2025-02-02 23:52:39 +0200

    feat: add Monthly spending limit and display dynamic progress bar based on currents month expenses

--- src/services/expenseService.ts
+++ src/services/expenseService.ts
@@ -1,5 +1,5 @@
 import { db } from "@/config/firebase";
-import { collection, addDoc, getDocs, query, orderBy, Timestamp } from "firebase/firestore";
+import { collection, addDoc, getDocs, query, orderBy, Timestamp, where, sum, getAggregateFromServer } from "firebase/firestore";
 
 interface Expense {
   amount: string;
@@ -12,15 +12,13 @@ interface Expense {
 
 export type DirectionOrder = "asc" | "desc";
 
-const userId = localStorage.getItem("uid");
-
-export const addExpense = async (expense: Expense) => {
+export const addExpense = async (userId: string, expense: Expense) => {
   const response = await addDoc(collection(db, "users", userId as string, "expenses"),
     { ...expense, uid: userId }
   )
 }
 
-export const getExpenses = async (type: string, order: DirectionOrder): Promise<(Expense & { docId: string })[] | null> => {
+export const getExpenses = async (userId: string, type: string, order: DirectionOrder): Promise<(Expense & { docId: string })[] | null> => {
   const q = query(collection(db, "users", userId as string, "expenses"), orderBy(type, order))
   const querySnapshot = await getDocs(q)
     .then(snapshot => {
@@ -30,6 +28,19 @@ export const getExpenses = async (type: string, order: DirectionOrder): Promise<
         return null;
       }
     })
+    .catch(e => {
+      console.log(e);
+      return null;
+    })
+  return querySnapshot
+}
+
+export const getExpensesForTheCurrentMonthHandler = async (userId: string, fromDate: Timestamp, toDate: Timestamp) => {
+  const q = query(collection(db, "users", userId as string, "expenses"), where("date", ">=", fromDate), where("date", "<=", toDate))
+  const querySnapshot = await getAggregateFromServer(q, {
+    totalExpenses: sum("amount")
+  })
+    .then(snap => snap.data().totalExpenses)
     .catch(e => {
       console.log(e);
       return null;
