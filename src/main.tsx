157d01d7c1bf84620db44bb24e129f23882dc7c1
DevNik14 <nykyurchiyski@gmail.com>
2025-02-04 23:25:08 +0200

    feat: successfully integrated tanstack query and fetched recent expenses

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
 import { AuthProvider } from "./contexts/authContext.tsx";
+
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import "./index.css";
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <QueryClientProvider client={client}>
+          <App />
+        </QueryClientProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
